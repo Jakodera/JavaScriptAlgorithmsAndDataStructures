42d0fb1c4fa5fb5f63876742a91570036a954766
Jakodera <you@example.com>
2021-09-16 20:15:27 +0300

    2. variables

---
+++ variables.js
@@ -0,0 +1,17 @@
+//Data is anything that is meaningful to the computer
+//Javascript data types
+/*
+     undefined, 
+     null, 
+     boolean,
+     string, 
+     symbol, 
+     bigint, 
+     number, 
+     and object.
+*/
+//Variables allow computers to store and manipulate data in a dynamic fashion
+
+//declaring a variable
+
+var myName;
