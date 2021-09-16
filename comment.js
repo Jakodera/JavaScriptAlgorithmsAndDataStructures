64869bc7a11b2b48a92ab19d61dc175cd3fec7df
Jakodera <you@example.com>
2021-09-16 19:41:34 +0300

    comments

---
+++ comment.js
@@ -0,0 +1,8 @@
+
+
+//single and multiline comments
+
+
+/*Comments are lines of code that JavaScript will intentionally ignore. 
+Comments are a great way to leave notes to yourself and to other people who will later need
+ to figure out what that code does.*/
