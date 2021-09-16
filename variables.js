8282590ef9b01eba26f0fa904e070fa5665eb7fb
Jakodera <you@example.com>
2021-09-16 20:58:24 +0300

    Declared variables

--- variables.js
+++ variables.js
@@ -7,11 +7,18 @@
      string, 
      symbol, 
      bigint, 
-     number, 
+     number -is a data type in JavaScript which represents numeric data, 
      and object.
 */
 //Variables allow computers to store and manipulate data in a dynamic fashion
 
-//declaring a variable
+/*Write variable names in JavaScript in camelCase. In camelCase, multi-word variable names 
+have the first word in lowercase and the first letter of each subsequent word is capitalized*/
+
+//declaring a variable and assigning value
 
 var myName;
+
+//Declare String Variables
+
+myName = "Jakodera"; //This assigns the string 'Jakodera' to myVariable.
